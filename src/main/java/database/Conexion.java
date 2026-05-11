13d49cf960fe86e4d7ed0ac1d4f99ae46f84beac
Josué Torres <josuejesusts@gmail.com>
2026-05-11 12:46:24 -0500

    refactor: re-estructuración de la base de datos

--- src/main/java/database/Conexion.java
+++ src/main/java/database/Conexion.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package database;
 
-import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Conexion {
 
@@ -15,24 +11,21 @@ public class Conexion {
         Connection con = null;
 
         try {
+            Dotenv dotenv = Dotenv.load();
 
-            String url = "URL_BD";
-            String user = "USUARIO_BD";
-            String password = "PASSWORD_BD";
-
+            String url = dotenv.get("DB_URL"); 
+            String user = dotenv.get("DB_USER");
+            String password = dotenv.get("DB_PASS");
 
             con = DriverManager.getConnection(url, user, password);
 
             System.out.println("Conexion exitosa a MySQL nube");
 
         } catch (Exception e) {
-
             System.out.println("Error de conexion");
             e.printStackTrace();
-
         }
 
         return con;
     }
 }
-
